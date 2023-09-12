db3623ae9b48a2ed1d9b5d26e77432002e849fdc
knifflig <118061175+knifflig@users.noreply.github.com>
2023-09-12 13:38:56 +0000

    new init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,8 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import tailwind from "@astrojs/tailwind"
-import basicSsl from '@vitejs/plugin-basic-ssl'
-const env = loadEnv("", process.cwd(), 'STORYBLOK')
- 
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-    }),
-    tailwind()
-  ],
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
-  },
-})
+  integrations: [tailwind()]
+});

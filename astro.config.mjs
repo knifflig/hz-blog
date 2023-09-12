92ba3a61cfb78969d65015fd40c520c7a249f30f
knifflig <118061175+knifflig@users.noreply.github.com>
2023-09-12 15:14:12 +0000

    reinit storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
-
+import storyblok from '@storyblok/astro'
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    storyblok({
+      accessToken: "ejSgZvoNxQNCkH421Dx7Nwtt",
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind()
+  ]
 });
